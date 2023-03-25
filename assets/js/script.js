310f03dedbb1207b48f8f7212bca393275cac2ff
Matheus Mendes <matheusjp03.mm@gmail.com>
2023-03-25 13:32:19 -0300

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,30 @@ function exibirEscolas() {
     var botao_adulto = document.getElementById("button-adulto").style.borderBottom = 'none';
     var botao_escolas = document.getElementById("button-escolar").style.borderBottom = 'solid 1px var(--red-color)';
 }
+
+function exibirPartidas() {
+    var display_partidas = document.getElementById('div-partidas').style.display = 'block';
+    var display_classificacao = document.getElementById("div-classificacao").style.display = 'none';
+    var display_news = document.getElementById("div-news").style.display = 'none';
+    var botao_partidas = document.getElementById("botao-partidas").style.borderBottom = 'solid 3px var(--white-color)';
+    var botao_classificacao = document.getElementById("botao-classificacao").style.borderBottom = 'none';
+    var botao_news = document.getElementById('botao-news').style.borderBottom = "none";
+}
+
+function exibirClassificacao() {
+    var display_partidas = document.getElementById('div-partidas').style.display = 'none';
+    var display_classificacao = document.getElementById("div-classificacao").style.display = 'block';
+    var display_news = document.getElementById("div-news").style.display = 'none';
+    var botao_partidas = document.getElementById("botao-partidas").style.borderBottom = "none";
+    var botao_classificacao = document.getElementById("botao-classificacao").style.borderBottom = 'solid 3px var(--white-color)';
+    var botao_news = document.getElementById('botao-news').style.borderBottom = "none";
+}
+
+function exibirNews() {
+    var display_partidas = document.getElementById('div-partidas').style.display = 'none';
+    var display_classificacao = document.getElementById("div-classificacao").style.display = 'none';
+    var display_news = document.getElementById("div-news").style.display = 'block';
+    var botao_classificacao = document.getElementById("botao-classificacao").style.borderBottom = 'none';
+    var botao_partidas = document.getElementById("botao-partidas").style.borderBottom = "none";
+    var botao_news = document.getElementById('botao-news').style.borderBottom = "solid 3px var(--white-color)";
+}
